0e300b7587513710785ec9195837e872b5b930b4
danialwafiy <danialwafiy1014@gmail.com>
2021-07-08 19:28:14 +0800

    Added json api documentation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                     </div>
                     <ul class="text-lg font-semibold">
                         <li class="px-6 py-2 bg-gray-900 text-white">API</li>
-                        <li class="px-6 py-2 border-t-2  border-gray-200"><a href="#playtime">Playtime</a></li>
+                        <li class="px-6 py-2 border-t-2  border-gray-200"><a href="#json">JSON</a></li>
                     </ul>
                 </div>
                 <div class="w-full h-full p-10">
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,19 +9,54 @@
         <div class="text-3xl font-bold">TOKEN</div>
         <div class="mt-1">A token is an alphanumeric code needed to access an API.</div>
         <div class="mt-1 font-semibold italic">How do I get the token ?</div>
-        <div class="mt-1">You can get the token by making a <span class="bg-gray-600 rounded p-1 text-white text-xs">POST</span> request to: http://mmu.ddns.net/api/token with your <span class="font-semibold">email and password </span>credentials as parameters.</div>
+        <div class="mt-1">You can get the token by making a <span class="bg-gray-600 rounded p-1 text-white text-xs">POST</span> request to: <b>http://mmu.ddns.net/api/token</b> with your <span class="font-semibold">email and password </span>credentials as parameters.</div>
         <div class="mt-1">For security reasons, <span class="text-red-500 font-semibold">DO NOT EXPOSE</span> your token to the public. After receiving the token, be sure to include it in your request headers:</div>
-       <pre class="mt-1">{'Authorization': 'token'}</pre>
+       <pre class="mt-1">{'Authorization': 'Bearer token'}</pre>
     </div>
     <div class="mb-8">
         <div class="text-3xl font-bold">API</div>
         <div class="mt-2 mb-4">Below is a list of the APIs available for the content team.</div>
-        <div class="mt-4" id="playtime">
-            <div class="italic bg-gray-900 text-white py-1 px-2 rounded"><span class=" font-semibold"'>PLAYTIME</span> - The total duration the user has been playing for a session.</div>
+        <div class="mt-4" id="json">
+            <div class="italic bg-gray-900 text-white py-1 px-2 rounded"><span class=" font-semibold"'>JSON</span> - Manage JSON data.</div>
             <div class="md:px-10 px-0 mt-4">
                 <div class="flex flex-wrap mb-8">
                     <div class="w-1/2 flex flex-col">                
-                        <div class=" my-2"><span class=" font-semibold">CREATE : <span class="bg-gray-600 rounded p-1 text-white text-xs">POST</span> http://mmu.ddns.net/playtime</div>
+                        <div class=" my-2"><span class=" font-semibold">RECEIVE: <span class="bg-gray-600 rounded p-1 text-white text-xs">GET</span> http://mmu.ddns.net/api/json</div>
+                        <div class=" my-2"><span class=" font-semibold">PARAMETERS :</span></div>
+                        <table class="border-2 border-gray-400 text-center w-1/2">
+                            <thead class="border-2 border-gray-400">
+                                <tr>
+                                    <th class="px-6 ">#</th>
+                                    <th class="px-6 text-sm ">Name</th>
+                                    <th class="px-6 text-sm ">Type</th>
+                                    <th class="px-6 text-sm">Required</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                                <tr class="border-2 border-gray-400">
+                                    <td colspan="4">-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="flex-1 md:mt-0 mt-3">
+                        <div class=" my-2"><span class=" font-semibold">SAMPLE RESPONSE :</span></div>
+                        <pre>
+[
+    {
+        "id": 1,
+        "json": "{"foo":"bar"}",
+        "created_at": "2021-07-06 10:21:42",
+        "updated_at": "2021-07-06 10:21:42"
+    },
+]
+                        </pre>
+                    </div>
+                </div>
+                <hr>
+                <div class="flex flex-wrap my-8">
+                    <div class="w-1/2 flex flex-col">                
+                        <div class=" my-2"><span class=" font-semibold">CREATE: <span class="bg-gray-600 rounded p-1 text-white text-xs">POST</span> http://mmu.ddns.net/api/json</div>
                         <div class=" my-2"><span class=" font-semibold">PARAMETERS :</span></div>
                         <table class="border-2 border-gray-400 text-center w-1/2">
                             <thead class="border-2 border-gray-400">
@@ -35,8 +70,8 @@
                             <tbody class="bg-white">
                                 <tr class="border-2 border-gray-400">
                                     <td class="px-6 ">1</td>
-                                    <td class="px-6 text-sm ">user_id</td>
-                                    <td class="px-6 text-sm ">Integer</td>
+                                    <td class="px-6 text-sm ">json</td>
+                                    <td class="px-6 text-sm ">JSON</td>
                                     <td class="px-6 text-sm"><i class="lni lni-checkmark text-lg"></i></td>
                                 </tr>
                             </tbody>
@@ -46,12 +81,87 @@
                         <div class=" my-2"><span class=" font-semibold">SAMPLE RESPONSE :</span></div>
                         <pre>
 {
-    status: 'Successfull'
+    "status": "success",
+    "data": {
+        "id": 1,
+        "json": "{\"foo\":\"bar\"}",
+        "updated_at": "2021-07-08 18:49:25",
+        "created_at": "2021-07-08 18:49:25",
+    }
 }
                         </pre>
                     </div>
                 </div>
                 <hr>
+                <div class="flex flex-wrap my-8">
+                    <div class="w-1/2 flex flex-col">                
+                        <div class=" my-2"><span class=" font-semibold">UPDATE: <span class="bg-gray-600 rounded p-1 text-white text-xs">PUT/PATCH</span> http://mmu.ddns.net/api/json/{json_id}</div>
+                        <div class=" my-2"><span class=" font-semibold">PARAMETERS :</span></div>
+                        <table class="border-2 border-gray-400 text-center w-1/2">
+                            <thead class="border-2 border-gray-400">
+                                <tr>
+                                    <th class="px-6 ">#</th>
+                                    <th class="px-6 text-sm ">Name</th>
+                                    <th class="px-6 text-sm ">Type</th>
+                                    <th class="px-6 text-sm">Required</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                                <tr class="border-2 border-gray-400">
+                                    <td class="px-6 ">1</td>
+                                    <td class="px-6 text-sm ">json</td>
+                                    <td class="px-6 text-sm ">JSON</td>
+                                    <td class="px-6 text-sm"><i class="lni lni-checkmark text-lg"></i></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="flex-1 md:mt-0 mt-3">
+                        <div class=" my-2"><span class=" font-semibold">SAMPLE RESPONSE :</span></div>
+                        <pre>
+{
+    "status": "success",
+    "data": {
+        "id": 1,
+        "json": "{\"foo\":\"bar\"}",
+        "updated_at": "2021-07-08 18:49:25",
+        "created_at": "2021-07-08 18:49:25",
+    }
+}
+ 
+                        </pre>
+                    </div>
+                </div>
+                <hr>
+                <div class="flex flex-wrap my-8">
+                    <div class="w-1/2 flex flex-col">                
+                        <div class=" my-2"><span class=" font-semibold">DELETE: <span class="bg-gray-600 rounded p-1 text-white text-xs">DELETE</span> http://mmu.ddns.net/api/json/{json_id}</div>
+                        <div class=" my-2"><span class=" font-semibold">PARAMETERS :</span></div>
+                        <table class="border-2 border-gray-400 text-center w-1/2">
+                            <thead class="border-2 border-gray-400">
+                                <tr>
+                                    <th class="px-6 ">#</th>
+                                    <th class="px-6 text-sm ">Name</th>
+                                    <th class="px-6 text-sm ">Type</th>
+                                    <th class="px-6 text-sm">Required</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                                <tr class="border-2 border-gray-400">
+                                    <td colspan="4">-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="flex-1 md:mt-0 mt-3">
+                        <div class=" my-2"><span class=" font-semibold">SAMPLE RESPONSE :</span></div>
+                        <pre>
+{
+    "status": "success"
+}
+                        </pre>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
